a80eaf06b879027dfb8f4b85fb0b05937e46698f
Johan Pettersson <xsnudelx@hotmail.com>
2016-01-09 15:35:35 +0100

    init

---
+++ WebApplication9/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication9.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public DateTime LastLogin { get; set; }
+        public DateTime Register { get; set; }
+        public String Status { get; set; }
+    }
+}
